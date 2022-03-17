c14c33d8f2d7e3a1fdce723663fcc0dadc7fba4e
mariosk <11368054+koulakis@users.noreply.github.com>
2022-03-18 00:09:35 +0100

    Docs 6.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@ import os
 import sys
 import sphinx_rtd_theme
 
-sys.path.insert(0, os.path.abspath('../../hnne'))
+sys.path.insert(0, os.path.abspath('../..'))
 
 # -- Project information -----------------------------------------------------
 
