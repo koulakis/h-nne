ba5971b887bf8bb69d587677e3adbee248757ec7
mariosk <11368054+koulakis@users.noreply.github.com>
2022-03-17 22:58:42 +0100

    Update version.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.rst").read_text()
 
 setuptools.setup(
     name="hnne",
-    version="0.1.3",
+    version="0.1.4",
     author="Marios Koulakis, Saquib Sarfraz",
     author_email="marios.koulakis@gmail.com",
     description="A fast hierarchical dimensionality reduction algorithm.",
