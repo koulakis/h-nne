3c2bff4d081a87bd8f88b1e60c9946b7d35c63a5
marios <RANDOM_EMAIL>
2025-10-20 13:34:52 +0200

    Fix all pre-commit issues.

--- benchmarking/evaluation.py
+++ benchmarking/evaluation.py
@@ -1,28 +1,29 @@
 import numpy as np
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.model_selection import StratifiedKFold
-from sklearn.metrics import accuracy_score
-from sklearn.manifold import trustworthiness
-from tqdm import tqdm
 from pynndescent import NNDescent
 from scipy.stats import mode
+from sklearn.manifold import trustworthiness
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import StratifiedKFold
+from sklearn.neighbors import KNeighborsClassifier
+from tqdm import tqdm
 
 
 def dim_reduction_benchmark(
-        k_values, 
-        data, 
-        embedding, 
-        labels, 
-        n_splits=10, 
-        shuffle=True,
-        sample_train_set=300000,
-        compute_trustworthiness=False,
-        use_ann=False,
-        seed=5536):
+    k_values,
+    data,
+    embedding,
+    labels,
+    n_splits=10,
+    shuffle=True,
+    sample_train_set=300000,
+    compute_trustworthiness=False,
+    use_ann=False,
+    seed=5536,
+):
     """Run a benchmark on the embeddings of a dataset in a lower dimension. The benchmark
     runs for a list of different number of k nearest neighbors and returns for each setting
     the accuracy and trustworthiness of the embedding.
-    
+
     Args:
         - k_values: a list of different number of nearest neighbors to evaluate on
         - data: a numpy array with the raw data with dimensions (n_datapoints, n_features)
@@ -33,7 +34,7 @@ def dim_reduction_benchmark(
         - shuffle: if true, the data will be shuffled before performing the splits
         - compute_trustworthiness: flag to disable expensive computation of trustworthiness
         - seed: seed to fix the dataset shuffle
-        
+
     Returns:
         - accuracies: a dictionary of the form {k: accuracies}, where k is a number of
             nearest neighbors and accuracies are the corresponding accuracies from the
@@ -43,10 +44,13 @@ def dim_reduction_benchmark(
     """
     kf = StratifiedKFold(n_splits=n_splits, shuffle=shuffle, random_state=seed)
     accuracies = dict()
-    
+
     if 0.9 * data.shape[0] > sample_train_set:
-        print(f'Will subsample train datasets from {0.9 * data.shape[0]} to {sample_train_set} for {k_values}-nn evaluation.')
-    
+        print(
+            f"Will subsample train datasets from {0.9 * data.shape[0]} to {sample_train_set} "
+            f"for {k_values}-nn evaluation."
+        )
+
     with tqdm(total=len(k_values) * kf.n_splits) as pbar:
         for i, k in enumerate(k_values):
             k_embedding = embedding
@@ -54,16 +58,18 @@ def dim_reduction_benchmark(
 
             for train_index, test_index in kf.split(k_embedding, labels):
                 if 0.9 * data.shape[0] > sample_train_set:
-                    train_index = np.random.choice(train_index, size=sample_train_set, replace=False)
+                    train_index = np.random.choice(
+                        train_index, size=sample_train_set, replace=False
+                    )
                 E_train, E_test = k_embedding[train_index], k_embedding[test_index]
 
                 y_train, y_test = labels[train_index], labels[test_index]
-                
+
                 if use_ann:
                     knn_index = NNDescent(E_train, n_trees=16, verbose=True)
                     pred_test_idx, _ = knn_index.query(E_test, k=k)
                     pred_test = mode(y_train[pred_test_idx], axis=1)[0].flatten()
-                    
+
                 else:
                     knn = KNeighborsClassifier(n_neighbors=k, n_jobs=-1)
                     knn.fit(E_train, y_train)
@@ -73,29 +79,32 @@ def dim_reduction_benchmark(
                 accuracy = accuracy_score(y_test, pred_test)
                 accuracies[k].append(accuracy)
                 pbar.update(1)
-                
+
             accuracies[k] = np.array(accuracies[k])
-                
+
         if compute_trustworthiness:
             if data.shape[0] < 20000:
                 tw = trustworthiness(data, embedding, n_neighbors=5)
-            else:                
+            else:
                 data_index = np.random.choice(data.shape[0], size=20000, replace=False)
-                tw = trustworthiness(data[data_index], k_embedding[data_index], n_neighbors=5)
+                tw = trustworthiness(
+                    data[data_index], k_embedding[data_index], n_neighbors=5
+                )
         else:
             tw = None
-                
+
     return accuracies, tw
 
 
 def format_metric(metric_values, digit_precision=3):
     """Format an output metric of the benchmark function.
-    
+
     Args:
         metric_values: a dictionary of the form {k: value}, where k is a number
             of nearest neighbors and value is the corresponding value of the metric
         digit_precision: the precision used to print the metric value and its std
     """
     return {
-        k: f'{value.mean():.{digit_precision}f} (\u00B1{value.std():.{digit_precision}f})' 
-        for k, value in metric_values.items()}
+        k: f"{value.mean():.{digit_precision}f} (\u00B1{value.std():.{digit_precision}f})"
+        for k, value in metric_values.items()
+    }
